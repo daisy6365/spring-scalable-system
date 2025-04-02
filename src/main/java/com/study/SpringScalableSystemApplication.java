8abdd4b8977e726dcd7a6d792a3eb1715e137baa
daisy6365 <gksekqls9808@inu.ac.kr>
2025-04-02 16:40:33 +0900

    init: project 생성

---
+++ src/main/java/com/study/SpringScalableSystemApplication.java
@@ -0,0 +1,13 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScalableSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringScalableSystemApplication.class, args);
+    }
+
+}

4653539cda8bd69430d4686199aff6aa9e24cc25
daisy6365 <gksekqls9808@inu.ac.kr>
2025-04-03 11:04:03 +0900

    feat: multimodule - 게시글조회 서비스 분리 생성

---
+++ microservice/article-read/src/test/java/com/study/articleread/ArticleReadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.articleread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleReadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

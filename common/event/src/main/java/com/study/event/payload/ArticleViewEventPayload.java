b685e59e96caa7956672e1d1426cb41a2a55d9da
daisy6365 <gksekqls9808@naver.com>
2026-02-28 13:19:53 +0900

    remove: 미사용코드 제거

--- common/event/src/main/java/com/study/event/payload/ArticleViewEventPayload.java
+++ common/event/src/main/java/com/study/event/payload/ArticleViewEventPayload.java
@@ -2,7 +2,6 @@ package com.study.event.payload;
 
 import com.study.event.EventPayload;
 
-import java.time.LocalDateTime;
 
 public class ArticleViewEventPayload implements EventPayload {
     private Long articleId;
